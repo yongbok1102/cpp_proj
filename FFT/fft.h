4ec15853766efcda073cad1cf1ec4acef6a81717
yongbok1102 <yongbok1102@gmail.com>
2017-02-07 17:26:48 +0900

    Revert "FFT"
    This reverts commit 72659b0bf2fb8b48f16f2f83215c4915358c9d29.

--- FFT/fft.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include <ctime>
-#define pi 3.1415926535898;
-using namespace std;
-double magn(double x, double y);
-void FFT(double x[], double dt, int N);
