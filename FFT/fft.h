8985bc4615c81ae5c9f6b76d6046d187fd3da0ce
yongbok1102 <yongbok1102@gmail.com>
2017-02-08 23:00:27 +0900

    FFT
    source codes for Fast Fourier Transform

---
+++ FFT/fft.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#define pi 3.1415926535898;
+using namespace std;
+double magn(double x, double y);
+void FFT(double x[], double dt, int N);
---
+++ FFT/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include "fft.h"
+
+#define pi 3.1415926535898;
+
+using namespace std;
+
+
+int main(){
+	ifstream is;
+    is.open("samp.dat");
+    if (is.bad()) {
+        return -1;
+    }
+	double T,X;
+	int N=0;
+    is.ignore(256, '\n'); // ignoring first line
+    while(!(is.fail())){
+        is >> T >> X;
+        N++;
+    }
+	is.close();
+	is.clear();
+	int n = N-1;
+
+	double* t = new double[n];
+	double* x = new double[n];
+	is.open("samp.dat");
+	is.ignore(256, '\n');	
+	for(int i=0; i<n; i++){
+        is >> t[i] >> x[i];		
+	}
+	is.close();	
+	double dt = t[1]-t[0];
+    FFT(x,dt,n);
+
+    return 0;
+}
